9fc02b7faec516034d120f866bc079ec8fc1ebcf
Verma7890 <137143631+Verma7890@users.noreply.github.com>
2023-07-23 08:30:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,8 +43,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Mrblackgod:nehalsingh969797@cluster0.gejltut.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Spider")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://filmycloud2:Rohit7890@cluster0.8qe4ahy.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Ironman")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
